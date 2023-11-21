4eedc54cdc4092c28c3f10447c94daf4f7659d4b
Safiyya <s.onanuga@reply.com>
2023-11-21 11:56:00 +0000

    React files init

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,53 @@
-<!DOCTYPE html>
-<html lang="en" class="govuk-template app-html-class">
-  <head>
-    <meta charset="utf-8" />
-    <title>Visit Someone In Prison</title>
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, viewport-fit=cover"
-    />
-    <meta name="theme-color" content="blue" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link
-      rel="shortcut icon"
-      sizes="16x16 32x32 48x48"
-      href="assets/images/favicon.ico"
-      type="image/x-icon"
-    />
-    <link rel="icon" href="assets/images/favicon.ico" />
-    <link
-      rel="mask-icon"
-      href="assets/images/govuk-mask-icon.svg"
-      color="blue"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="assets/images/govuk-apple-touch-icon-180x180.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="167x167"
-      href="assets/images/govuk-apple-touch-icon-167x167.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="152x152"
-      href="assets/images/govuk-apple-touch-icon-152x152.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      href="assets/images/govuk-apple-touch-icon.png"
-    />
-    <link href="styles/govuk-frontend-4.7.0.min.css" rel="stylesheet" />
-    <meta
-      property="og:image"
-      content="&lt;YOUR-DOMAIN&gt;/images/govuk-opengraph-image.png"
-    />
-  </head>
+import { Page, BackLink, TopNav, Crown, H1, GlobalStyle, H2, Fieldset, GridRow, GridCol, FormGroup, Heading, Label, LabelText, HintText, Input, DateField, Select, Button } from "govuk-react";
 
-  <body
-    class="govuk-template__body app-body-class"
-    data-test="My value"
-    data-other="report:details"
-  >
-    <script>
-      document.body.className = document.body.className
-        ? document.body.className + " js-enabled"
-        : "js-enabled";
-    </script>
-    <a
-      href="#main-content"
-      class="govuk-skip-link"
-      data-module="govuk-skip-link"
-      >Skip to main content</a
+export default function FirstPage() {
+  return (
+    <Page
+      header={<TopNav />}
     >
-    <header class="govuk-header" role="banner" data-module="govuk-header">
-      <div class="govuk-header__container app-width-container">
-        <div class="govuk-header__logo">
-          <a href="#" class="govuk-header__link govuk-header__link--homepage">
-            <span class="govuk-header__logotype">
-              <!--[if gt IE 8]><!-->
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                class="govuk-header__logotype-crown"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 132 97"
-                height="30"
-                width="36"
+      <H1>
+        Visit Someone in Prison
+      </H1>
+
+
+      <GridRow>
+        <GridCol setWidth="two thirds">
+          <FormGroup>
+            <Heading size="MEDIUM">Prisoner details</Heading>
+          </FormGroup>
+          <Fieldset>
+            <FormGroup>
+              <Label>
+                <LabelText>Prisoner first name</LabelText>
+                <Input />
+              </Label>
+            </FormGroup>
+            <FormGroup>
+              <Label>
+                <LabelText>Prisoner last name</LabelText>
+                <Input />
+              </Label>
+            </FormGroup>
+            <FormGroup>
+              <DateField
+                hintText="For example, 29 04 1966"
               >
-                <path
-                  fill="currentColor"
-                  fill-rule="evenodd"
-                  d="M25 30.2c3.5 1.5 7.7-.2 9.1-3.7 1.5-3.6-.2-7.8-3.9-9.2-3.6-1.4-7.6.3-9.1 3.9-1.4 3.5.3 7.5 3.9 9zM9 39.5c3.6 1.5 7.8-.2 9.2-3.7 1.5-3.6-.2-7.8-3.9-9.1-3.6-1.5-7.6.2-9.1 3.8-1.4 3.5.3 7.5 3.8 9zM4.4 57.2c3.5 1.5 7.7-.2 9.1-3.8 1.5-3.6-.2-7.7-3.9-9.1-3.5-1.5-7.6.3-9.1 3.8-1.4 3.5.3 7.6 3.9 9.1zm38.3-21.4c3.5 1.5 7.7-.2 9.1-3.8 1.5-3.6-.2-7.7-3.9-9.1-3.6-1.5-7.6.3-9.1 3.8-1.3 3.6.4 7.7 3.9 9.1zm64.4-5.6c-3.6 1.5-7.8-.2-9.1-3.7-1.5-3.6.2-7.8 3.8-9.2 3.6-1.4 7.7.3 9.2 3.9 1.3 3.5-.4 7.5-3.9 9zm15.9 9.3c-3.6 1.5-7.7-.2-9.1-3.7-1.5-3.6.2-7.8 3.7-9.1 3.6-1.5 7.7.2 9.2 3.8 1.5 3.5-.3 7.5-3.8 9zm4.7 17.7c-3.6 1.5-7.8-.2-9.2-3.8-1.5-3.6.2-7.7 3.9-9.1 3.6-1.5 7.7.3 9.2 3.8 1.3 3.5-.4 7.6-3.9 9.1zM89.3 35.8c-3.6 1.5-7.8-.2-9.2-3.8-1.4-3.6.2-7.7 3.9-9.1 3.6-1.5 7.7.3 9.2 3.8 1.4 3.6-.3 7.7-3.9 9.1zM69.7 17.7l8.9 4.7V9.3l-8.9 2.8c-.2-.3-.5-.6-.9-.9L72.4 0H59.6l3.5 11.2c-.3.3-.6.5-.9.9l-8.8-2.8v13.1l8.8-4.7c.3.3.6.7.9.9l-5 15.4v.1c-.2.8-.4 1.6-.4 2.4 0 4.1 3.1 7.5 7 8.1h.2c.3 0 .7.1 1 .1.4 0 .7 0 1-.1h.2c4-.6 7.1-4.1 7.1-8.1 0-.8-.1-1.7-.4-2.4V34l-5.1-15.4c.4-.2.7-.6 1-.9zM66 92.8c16.9 0 32.8 1.1 47.1 3.2 4-16.9 8.9-26.7 14-33.5l-9.6-3.4c1 4.9 1.1 7.2 0 10.2-1.5-1.4-3-4.3-4.2-8.7L108.6 76c2.8-2 5-3.2 7.5-3.3-4.4 9.4-10 11.9-13.6 11.2-4.3-.8-6.3-4.6-5.6-7.9 1-4.7 5.7-5.9 8-.5 4.3-8.7-3-11.4-7.6-8.8 7.1-7.2 7.9-13.5 2.1-21.1-8 6.1-8.1 12.3-4.5 20.8-4.7-5.4-12.1-2.5-9.5 6.2 3.4-5.2 7.9-2 7.2 3.1-.6 4.3-6.4 7.8-13.5 7.2-10.3-.9-10.9-8-11.2-13.8 2.5-.5 7.1 1.8 11 7.3L80.2 60c-4.1 4.4-8 5.3-12.3 5.4 1.4-4.4 8-11.6 8-11.6H55.5s6.4 7.2 7.9 11.6c-4.2-.1-8-1-12.3-5.4l1.4 16.4c3.9-5.5 8.5-7.7 10.9-7.3-.3 5.8-.9 12.8-11.1 13.8-7.2.6-12.9-2.9-13.5-7.2-.7-5 3.8-8.3 7.1-3.1 2.7-8.7-4.6-11.6-9.4-6.2 3.7-8.5 3.6-14.7-4.6-20.8-5.8 7.6-5 13.9 2.2 21.1-4.7-2.6-11.9.1-7.7 8.8 2.3-5.5 7.1-4.2 8.1.5.7 3.3-1.3 7.1-5.7 7.9-3.5.7-9-1.8-13.5-11.2 2.5.1 4.7 1.3 7.5 3.3l-4.7-15.4c-1.2 4.4-2.7 7.2-4.3 8.7-1.1-3-.9-5.3 0-10.2l-9.5 3.4c5 6.9 9.9 16.7 14 33.5 14.8-2.1 30.8-3.2 47.7-3.2z"
-                ></path>
-              </svg>
-              <!--<![endif]-->
-              <!--[if IE 8]>
-                <img
-                  src="assets/images/govuk-logotype-crown.png"
-                  class="govuk-header__logotype-crown-fallback-image"
-                  width="36"
-                  height="32"
-                  alt=""
-                />
-              <![endif]-->
-              <span class="govuk-header__logotype-text"> GOV.UK </span>
-            </span>
-          </a>
-        </div>
-        <div class="govuk-header__content">
-          <a href="#" class="govuk-header__link govuk-header__service-name">
-            Service name
-          </a>
-        </div>
-      </div>
-    </header>
-    <div class="govuk-width-container app-width-container">
-      <main
-        class="govuk-main-wrapper app-main-class"
-        id="main-content"
-        role="main"
-      >
-        <h1 class="govuk-heading-xl">Who are you visiting?</h1>
-      </main>
-      <form
-        class="js-SubmitOnce"
-        id="new_prisoner_step"
-        autocomplete="off"
-        accept-charset="UTF-8"
-        method="post"
-      >
-        <div class="govuk-grid-row">
-          <div class="govuk-grid-column-two-thirds">
-            <div class="govuk-form-group">
-              <h2 class="govuk-heading-m">Prisoner details</h2>
-            </div>
-            <fieldset class="govuk-fieldset" role="group">
-              <div class="govuk-form-group">
-                <label class="govuk-label" for="prisoner_step_first_name"
-                  >Prisoner first name</label>
-                  <input
-                  class="govuk-input"
-                  type="text"
-                  name="prisoner_step[first_name]"
-                  id="prisoner_step_first_name"
-                />
-              </div>
-              <div class="govuk-form-group">
-                <label class="govuk-label" for="prisoner_step_last_name"
-                  >Prisoner last name</label
-                ><input
-                  class="govuk-input"
-                  type="text"
-                  name="prisoner_step[last_name]"
-                  id="prisoner_step_last_name"
-                />
-              </div>
-              <div class="govuk-form-group">
-                <fieldset class="govuk-fieldset" role="group">
-                    <label class="govuk-label" for="prisoner_step_last_name"
-                    >Prisoner date of birth</label>
-                    <span class="govuk-hint" id="date_of_birth_hint"
-                      >For example, 28 04 1996</span>
-                    <div class="govuk-date-input" id="passport-issued">
-                        <div class="govuk-date-input__item">
-                          <div class="govuk-form-group">
-                            <label class="govuk-label govuk-date-input__label" for="prisoner_step_date_of_birth_day">
-                              Day
-                            </label>
-                            <input class="govuk-input govuk-date-input__input govuk-input--width-2" id="prisoner_step_date_of_birth_day" name="prisoner_step_date_of_birth_day" type="text" inputmode="numeric">
-                          </div>
-                        </div>
-                        <div class="govuk-date-input__item">
-                          <div class="govuk-form-group">
-                            <label class="govuk-label govuk-date-input__label" for="prisoner_step_date_of_birth_month">
-                              Month
-                            </label>
-                            <input class="govuk-input govuk-date-input__input govuk-input--width-2" id="prisoner_step_date_of_birth_month" name="prisoner_step_date_of_birth_month" type="text" inputmode="numeric">
-                          </div>
-                        </div>
-                        <div class="govuk-date-input__item">
-                          <div class="govuk-form-group">
-                            <label class="govuk-label govuk-date-input__label" for="prisoner_step_date_of_birth_year">
-                              Year
-                            </label>
-                            <input class="govuk-input govuk-date-input__input govuk-input--width-4" id="prisoner_step_date_of_birth_year" name="prisoner_step_date_of_birth_year" type="text" inputmode="numeric">
-                          </div>
-                        </div>
-                      </div>
-                </fieldset>
-              </div>
-              <div class="govuk-form-group">
-                <fieldset class="govuk-fieldset" role="group">
-                <label class="govuk-label" for="prisoner_step_number">Prisoner number
-                </label>
-                <span class="govuk-hint" id="number_hint">For example, A1234BC</span>
-                <input
-                  class="govuk-input"
-                  type="text"
-                  name="prisoner_step[number]"
-                  id="prisoner_step_number"
-                />
-                </fieldset>
-              </div>
-              <div class="govuk-form-group">
-
-                <label class="govuk-label" for="prisoner_step_prison_id">Prison name</label>
-
-                <span class="govuk-hint" id="prison_id_hint">For example, Cardiff</span>
-                <select
-                  class="govuk-select"
-                  name="prisoner_step[prison_id]"
-                  id="prisoner_step_prison_id"
+                Prisoner date of birth
+              </DateField>
+            </FormGroup>
+            <FormGroup>
+              <Label>
+                <LabelText>Prisoner number</LabelText>
+                <HintText>For example, A1234BC</HintText>
+                <Input />
+              </Label>
+            </FormGroup>
+            <FormGroup>
+                <Select
+                  label="Prison name"
+                  hint="For example, Cardiff"
                 >
-                  <option value="">select a prison</option>
+                <option value="">select a prison</option>
                   <option value="a81df211-95a1-47bc-9db4-866d54040121">
                     Acklington
                   </option>
@@ -612,78 +456,16 @@
                   <option value="8484479f-f7f5-4002-a748-38d68b1bf43d">
                     Wymott
                   </option>
-                </select>
-              </div>
-              <div class="govuk-form-group">
-                <input
-                  type="submit"
-                  name="commit"
-                  value="Continue"
-                  class="govuk-button"
-                  data-disable-with="Continue"
-                />
-              </div>
-            </fieldset>
-          </div>
-        </div>
-      </form>
-    </div>
+                </Select>
+            </FormGroup>
+            <FormGroup>
+              <Button>Continue</Button>
+            </FormGroup>
+          </Fieldset>
+        </GridCol>
+      </GridRow>
 
-    <footer class="govuk-footer" role="contentinfo">
-      <div class="govuk-width-container">
-        <div class="govuk-footer__meta">
-          <div class="govuk-footer__meta-item govuk-footer__meta-item--grow">
-            <h2 class="govuk-visually-hidden">Support links</h2>
-            <ul class="govuk-footer__inline-list">
-              <li class="govuk-footer__inline-list-item">
-                <a class="govuk-footer__link" href="#"> Help </a>
-              </li>
-              <li class="govuk-footer__inline-list-item">
-                <a class="govuk-footer__link" href="#"> Cookies </a>
-              </li>
-              <li class="govuk-footer__inline-list-item">
-                <a class="govuk-footer__link" href="#"> Contact </a>
-              </li>
-              <li class="govuk-footer__inline-list-item">
-                <a class="govuk-footer__link" href="#">
-                  Terms and conditions
-                </a>
-              </li>
-            </ul>
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              class="govuk-footer__licence-logo"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 483.2 195.7"
-              height="17"
-              width="41"
-            >
-              <path
-                fill="currentColor"
-                d="M421.5 142.8V.1l-50.7 32.3v161.1h112.4v-50.7zm-122.3-9.6A47.12 47.12 0 0 1 221 97.8c0-26 21.1-47.1 47.1-47.1 16.7 0 31.4 8.7 39.7 21.8l42.7-27.2A97.63 97.63 0 0 0 268.1 0c-36.5 0-68.3 20.1-85.1 49.7A98 98 0 0 0 97.8 0C43.9 0 0 43.9 0 97.8s43.9 97.8 97.8 97.8c36.5 0 68.3-20.1 85.1-49.7a97.76 97.76 0 0 0 149.6 25.4l19.4 22.2h3v-87.8h-80l24.3 27.5zM97.8 145c-26 0-47.1-21.1-47.1-47.1s21.1-47.1 47.1-47.1 47.2 21 47.2 47S123.8 145 97.8 145"
-              />
-            </svg>
-            <span class="govuk-footer__licence-description">
-              All content is available under the
-              <a
-                class="govuk-footer__link"
-                href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
-                rel="license"
-                >Open Government Licence v3.0</a
-              >, except where otherwise stated
-            </span>
-          </div>
-          <div class="govuk-footer__meta-item">
-            <a
-              class="govuk-footer__link govuk-footer__copyright-logo"
-              href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/"
-              >Â© Crown copyright</a
-            >
-          </div>
-        </div>
-      </div>
-    </footer>
-    <script src="js/govuk-frontend-4.7.0.min.js"></script>
-  </body>
-</html>
+    </Page>
+      
+  );
+}

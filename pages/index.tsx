08ed44a651b93819220f59137017d47744b2eff7
kanishk-reply <160497361+kanishk-reply@users.noreply.github.com>
2024-02-27 12:30:13 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,32 +18,6 @@ import React, { useState, useEffect, FormEvent } from "react";
 import { useForm } from '@formspree/react';
 
 export default function FirstPage() {
-  // Define a translations object
-const translations = {
-    pageTitle: "Visit Someone in Prison",
-    prisonerDetailsHeading: "Prisoner Details",
-    firstNameLabel: "Prisoner First Name",
-    lastNameLabel: "Prisoner Last Name",
-    prisonerNumberLabel: "Prisoner Number",
-    prisonerNumberHint: "For example, A1234BC",
-    prisonNameLabel: "Prison Name",
-    prisonNameHint: "For example, Cardiff",
-    selectPrisonPlaceholder: "Select a Prison",
-    bullingdonConvicted: "Bullingdon (Convicted Only)",
-    bullingdonRemand: "Bullingdon (Remand Only)",
-    continueButton: "Continue",
-    enterFirstName: "Please enter a first name",
-    enterLastName: "Please enter a last name",
-    enterPrisonName: "Please enter a prison name",
-    enterValidPrisonerNumber: "Please enter a correctly formatted prisoner number",
-    enterPrisonerNumber: "Please enter a prisoner number",
-    dobLabel: "Prisoner's Date of Birth",
-    enterValidDob: "Please enter a valid date of birth (yyyy-mm-dd)"
-};
-
-  const imageURL = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-aGEVVU5tg2M2AsgFnKKY8QJR/user-fcv2C4iR96qYVhGJaLfvBrFl/img-NU5xcJIOtukL4RMhCr5cNkL9.png?st=2023-12-05T14%3A23%3A14Z&se=2023-12-05T16%3A23%3A14Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-05T13%3A34%3A06Z&ske=2023-12-06T13%3A34%3A06Z&sks=b&skv=2021-08-06&sig=NWXuzd28QPLZyXKuLt5T1PSG52tj8XOf7TLDZgyxY6M%3D"
-  const snowGIFUrl = "https://i.pinimg.com/originals/8b/30/71/8b3071feeff83f1e4a63ed231562ff0c.gif"
-
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastname] = useState("");
   const [prisonerNumber, setPrisonerNumber] = useState("");
@@ -51,29 +25,6 @@ const translations = {
   const [isFormValid, setIsFormValid] = useState(false);
   const [state, handleSubmit] = useForm("mnqkkwpq");
   const [buttonClicked, setButtonClicked] = useState(false);
-  const [prisonerDob, setPrisonerDob] = useState("");
-
-  const [timeUntilChristmas, setTimeUntilChristmas] = useState('');
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      const now = new Date();
-      const christmas = new Date(now.getFullYear(), 11, 25); // 11 is December in JavaScript Date
-      if (now.getMonth() === 11 && now.getDate() > 25) {
-        christmas.setFullYear(christmas.getFullYear() + 1);
-      }
-      const diff = christmas.getTime() - now.getTime();
-
-      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-      const minutes = Math.floor((diff / 1000 / 60) % 60);
-      const seconds = Math.floor((diff / 1000) % 60);
-
-      setTimeUntilChristmas(`${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`);
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
 
   if (state.succeeded) {
       console.log("form sent");
@@ -83,22 +34,11 @@ const translations = {
   }
 
   const validateFirstName: (value?: string) => string | undefined = (value) =>
-    value ? undefined : translations.enterFirstName;
+    value ? undefined : "Please enter a first name";
   const validateLastName: (value?: string) => string | undefined = (value) =>
-    value ? undefined : translations.enterLastName;
-
-  // Validation for prisoner's date of birth
-  const validatePrisonerDob: (value?: string) => string | undefined = (value) => {
-    // Simple validation: check if the date of birth is in a valid date format (yyyy-mm-dd)
-    const dobRegex = /^\d{4}-\d{2}-\d{2}$/;
-    if (value && dobRegex.test(value)) {
-      return undefined;
-    } else {
-      return translations.enterValidDob;
-    }
-  };
+    value ? undefined : "Please enter a last name";
   const validatePrisonName: (value?: string) => string | undefined = (value) =>
-  value ? undefined : translations.enterPrisonName;
+    value ? undefined : "Please enter a prison name";
 
   // prison number is a capital A followed by 4 numbers and 2 letters
   const validatePrisonNumber: (value?: string) => string | undefined = (
@@ -109,9 +49,9 @@ const translations = {
     if (value) {
       result = prisonerNumberRegex.test(value)
         ? undefined
-        : translations.enterValidPrisonerNumber;
+        : "Please enter a correctly formatted prisoner number";
     } else {
-      return translations.enterPrisonerNumber;
+      return "Please enter a prisoner number";
     }
     return result;
   };
@@ -121,8 +61,7 @@ const translations = {
       !validateFirstName(firstName) &&
       !validateLastName(lastName) &&
       !validatePrisonName(prisonName) &&
-      !validatePrisonNumber(prisonerNumber)&&
-      !validatePrisonerDob(prisonerDob)
+      !validatePrisonNumber(prisonerNumber)
     ) {
       setIsFormValid(true);
     } else {
@@ -151,42 +90,19 @@ const translations = {
   }
 
   return (
-    <div>
-    {/*
-    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-    <img src={imageURL} alt="DALL-E Generated Image" style={{ width: '50%', height: 'auto' }} />
-    <div style={{ width: '45%' }}>
-    */}
     <Page header={<TopNav />}>
-      {/*
-      <div style={{
-        textAlign: 'center',
-        padding: '15px',
-        backgroundColor: '#FF0000', // Christmas red background
-        color: '#FFFFFF', // White text
-        fontWeight: 'bold',
-        border: '5px dashed gold', // Gold border
-        borderRadius: '10px',
-        margin: '10px 0',
-        fontSize: '24px',
-        fontFamily: 'Arial, sans-serif',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' // subtle shadow
-      }}>
-        ðŸŽ„ Only {timeUntilChristmas} until Christmas! ðŸŽ…
-      </div>
-      */}
-      <H1>{translations.pageTitle}</H1>
+      <H1>Visit Someone in Prison</H1>
 
       <GridRow>
         <GridCol setWidth="two-thirds">
           <FormGroup>
-            <Heading size="MEDIUM">{translations.prisonerDetailsHeading}</Heading>
+            <Heading size="MEDIUM">Prisoner details</Heading>
           </FormGroup>
           <Fieldset>
             <form onSubmit={onSubmit} method="POST">
               <FormGroup error={buttonClicked && validateFirstName(firstName)}>
                 <Label>
-                  <LabelText>{translations.firstNameLabel}</LabelText>
+                  <LabelText>Prisoner first name</LabelText>
                   <Input
                     name="firstName"
                     value={firstName}
@@ -196,7 +112,7 @@ const translations = {
               </FormGroup>
               <FormGroup error={buttonClicked && validateLastName(lastName)}>
                 <Label>
-                  <LabelText>{translations.lastNameLabel}</LabelText>
+                  <LabelText>Prisoner last name</LabelText>
                   <Input
                     name="lastName"
                     value={lastName}
@@ -204,21 +120,10 @@ const translations = {
                   />
                 </Label>
               </FormGroup>
-              <FormGroup error={buttonClicked && validatePrisonerDob(prisonerDob)}>
-                <Label>
-                  <LabelText>{translations.dobLabel}</LabelText>
-                  <Input
-                    type="date"
-                    name="prisonerDob"
-                    value={prisonerDob}
-                    onChange={(e) => setPrisonerDob(e.target.value)}
-                  />
-                </Label>
-              </FormGroup>
               <FormGroup error={buttonClicked && validatePrisonNumber(prisonerNumber)}>
                 <Label>
-                  <LabelText>{translations.prisonerNumberLabel}</LabelText>
-                  <HintText>{translations.prisonerNumberHint}</HintText>
+                  <LabelText>Prisoner number</LabelText>
+                  <HintText>For example, A1234BC</HintText>
                   <Input
                     name="prisonNumber"
                     value={prisonerNumber}
@@ -228,8 +133,8 @@ const translations = {
               </FormGroup>
               <FormGroup error={buttonClicked && validatePrisonName(prisonName)}>
                 <Select
-                  label={translations.prisonNameLabel}
-                  hint={translations.prisonNameHint}
+                  label="Prison name"
+                  hint="For example, Cardiff"
                   
                   input={{
                     name:"prisonName",
@@ -237,7 +142,7 @@ const translations = {
                     onChange: (e) => setPrisonName(e.target.value),
                   }}
                 >
-                  <option value="">{translations.selectPrisonPlaceholder}</option>
+                  <option value="">Select a prison</option>
                     <option value="Acklington">
                       Acklington
                     </option>
@@ -284,10 +189,10 @@ const translations = {
                       Buckley Hall
                     </option>
                     <option value="Bullingdon (Convicted Only)">
-                      {translations.bullingdonConvicted}
+                      Bullingdon (Convicted Only)
                     </option>
                     <option value="Bullingdon (Remand Only)">
-                      {translations.bullingdonRemand}
+                      Bullingdon (Remand Only)
                     </option>
                     <option value="Bure">
                       Bure
@@ -298,28 +203,12 @@ const translations = {
                   </Select>
               </FormGroup>
               <FormGroup>
-                <Button type="submit" disabled={state.submitting}>{translations.continueButton}</Button>
+                <Button type="submit" disabled={state.submitting}>Continue</Button>
               </FormGroup>
             </form>
           </Fieldset>
         </GridCol>
       </GridRow>
     </Page>
-    {/*
-    </div>
-    </div>
-    */}
-    <div style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        backgroundImage: `url(${snowGIFUrl})`, // Using the online GIF URL
-        backgroundSize: 'cover',
-        pointerEvents: 'none',
-        zIndex: 1000 // Ensure this is above other content but below interactive elements
-      }} />
-    </div>
   );
 }
